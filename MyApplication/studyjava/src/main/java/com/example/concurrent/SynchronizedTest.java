1665fe7c80f2d0ed3ca34018b1e55dc1631172e7
zenobia <974670990@qq.com>
2016-05-27 16:56:19 +0800

    同步测试demo

---
+++ MyApplication/studyjava/src/main/java/com/example/concurrent/SynchronizedTest.java
@@ -0,0 +1,41 @@
+package com.example.concurrent;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by wanghui on 2016/5/27.
+ */
+public class SynchronizedTest {
+    private static int i;
+    public static void main(String[] args){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int j = 0; j < 10; j++){
+                    System.out.println("wh--thread1---i=" + i ++);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int j = 0; j < 10; j++){
+                    System.out.println("wh--thread2---i=" + i ++);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+    }
+}
