e4a0ac6aa028b364d3428337af30186fd74794b4
zenobia <974670990@qq.com>
2016-06-01 17:02:06 +0800

    ObservableSet

--- MyApplication/studyjava/src/main/java/com/example/concurrency/ObservableSet.java
+++ MyApplication/studyjava/src/main/java/com/example/concurrency/ObservableSet.java
@@ -1,7 +1,16 @@
 package com.example.concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by wanghui on 2016/5/31.
  */
-public class ObservableSet extends ForwardingSet {
+public class ObservableSet<E> extends ForwardingSet<E> {
+    public ObservableSet(Set<E> es) {
+        super(es);
+    }
+
+    private final List<ObservableSet<E>> observes = new ArrayList<>();
 }
